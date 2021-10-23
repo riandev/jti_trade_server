679118d3c7176db96a4218533079aeae238c2d15
Rashaduzamman Rian <rian.acca@gmail.com>
2021-10-23 13:35:58 +0600

    localk

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const _ = require("lodash");
 const path = require("path");
 
 const app = express();
-app.use(express.static("../jti_sheikh_lep_client/build"));
+app.use(express.static("../jti_trade_client/build"));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false, limit: "5000mb" }));
 app.use(bodyParser.json({ limit: "5000mb" }));
@@ -15,9 +15,9 @@ const port = 5056;
 
 const MongoClient = require("mongodb").MongoClient;
 // const MongoClient = require("mongodb").MongoClient;
-// const uri = "mongodb://127.0.0.1:27017/jti_trade";
-const uri =
-  "mongodb+srv://aktcl:01939773554op5t@cluster0.9akoo.mongodb.net/jti_trade?retryWrites=true&w=majority";
+const uri = "mongodb://127.0.0.1:27017/jti_trade";
+// const uri =
+//   "mongodb+srv://aktcl:01939773554op5t@cluster0.9akoo.mongodb.net/jti_trade?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -1601,11 +1601,11 @@ client.connect((err) => {
         res.send(results);
       });
   });
-  // app.get("*", (req, res) => {
-  //   res.sendFile(
-  //     path.join(__dirname, "../jti_sheikh_lep_client/build", "index.html")
-  //   );
-  // });
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../jti_trade_client/build", "index.html")
+    );
+  });
 });
 
 app.get("/", (req, res) => {
